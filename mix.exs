fe9f5be13360f23d5e6e7136b9b22947fccccfa3
JohnDoneth <doneth7@gmail.com>
2021-08-19 16:00:25 -0400

    Add native directory

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Exblockhash.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12-rc",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -23,6 +24,13 @@ defmodule Exblockhash.MixProject do
     [
       {:rustler, "~> 0.22.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      files: ~w(lib priv native .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src)
     ]
   end
 end
