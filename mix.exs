5d0b55a063be0e457326e3ed5845c4168ac1c749
JohnDoneth <doneth7@gmail.com>
2021-08-20 10:06:09 -0400

    Add support for M1 Macs and bump major version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exblockhash.MixProject do
   def project do
     [
       app: :exblockhash,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.12-rc",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
